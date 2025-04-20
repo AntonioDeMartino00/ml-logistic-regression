4a2a033a6f01376ff4b02efcc2b0dbdbbede3d52
Antonio <antoniodemartino@gmx.net>
2025-04-21 01:45:49 +0200

    Add functions to make predictions and display model coefficients

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,3 +1,12 @@
+import pandas as pd
+
+
 def make_predictions(model, X_test):
     y_pred = model.predict(X_test)
     return y_pred
+
+
+def display_model_coefficients(model, X_train):
+    coefficients = pd.DataFrame(model.coef_.flatten(), X_train.columns, columns=['Koeffizient'])
+    print(coefficients)
+    return coefficients
