322a11a1d5e0fb7f17c3fc7f9e23d5aaab77acf4
Antonio <antoniodemartino@gmx.net>
2025-04-21 20:47:00 +0200

    add evaluate_accuracy, evaluate_classification_metrics, evaluate_confusion_matrix functions to evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.metrics import confusion_matrix
 
 
 def make_predictions(model, X_test):
@@ -16,3 +19,29 @@ def compare_actual_vs_predicted(y_test, y_pred):
     comparison = pd.DataFrame({'Tatsächliche Werte': y_test, 'Vorhersagen': y_pred})
     print(comparison)
     return comparison
+
+
+def evaluate_accuracy(y_test, y_pred):
+    accuracy = accuracy_score(y_test, y_pred)
+    print("Accuracy:", accuracy)
+    return accuracy
+
+
+def evaluate_classification_metrics(y_test, y_pred):
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+
+    print(f"Precision: {precision}")
+    print(f"Recall: {recall}")
+    print(f"F1-Score: {f1}")
+
+    return {"precision": precision, "recall": recall, "f1": f1}
+
+
+def evaluate_confusion_matrix(y_test, y_pred):
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    print("Confusion Matrix:\n", conf_matrix)
+    return conf_matrix
+
+
