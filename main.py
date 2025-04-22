1bb7d2e7ba5e8cdca328dc573c6591d2668d877d
Antonio <antoniodemartino@gmx.net>
2025-04-22 16:42:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from src import data_loader.py
 from src import preprocess.py
-from src import 
+from src import visualize.py
+from src import feature_selection.py
+from src import data_split.py
+from src import model.py
+from src import evaluate.py
