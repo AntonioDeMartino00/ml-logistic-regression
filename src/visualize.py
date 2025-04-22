c9c71468ee1bedc8401f0fcd9110c25a313a79a8
Antonio <antoniodemartino@gmx.net>
2025-04-22 16:40:08 +0200

    Update visualize.py

--- src/visualize.py
+++ src/visualize.py
@@ -3,28 +3,28 @@ import seaborn as sns
 
 def plot_target_distribution(df):
     sns.countplot(x='HeartDisease', data=df)
-    plt.title('Verteilung der Zielvariable')
+    plt.title('target distribution')
     plt.show()
 
 def plot_age_distribution(df):
     sns.histplot(df['Age'], kde=True)
-    plt.title('Alter der Patienten')
+    plt.title('age distribution')
     plt.show()
 
 def plot_correlation_matrix(df):
     correlation_matrix = df.corr()
     plt.figure(figsize=(12, 8))
     sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
-    plt.title('Korrelationsmatrix')
+    plt.title('correlationmatrix')
     plt.show()
 
 def print_target_class_distribution(df):
     print(df['HeartDisease'].value_counts())
     
 def print_column_info(df):
-    print("Datentypen der Spalten:")
+    print("datatypes of the columns:")
     print(df.dtypes)
-    print("\nSpaltennamen:")
+    print("\ncolnames:")
     print(df.columns)
-    print("\nSpaltenliste:")
+    print("\ncollist:")
     print(df.columns.tolist())
